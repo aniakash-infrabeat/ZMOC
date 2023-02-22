bc6e8ffef833ff9ffb470f7ea1b17245ab85a9bd
aniakash-infrabeat <aniakash@infrabeat.com>
2023-02-22 05:12:40 +0000

    GIT Comment

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter","sap/ui/model/FilterOperator",
+    "sap/ui/core/Fragment","sap/ui/core/routing/History",
+    "sap/ui/core/UIComponent"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller,_JSONModel,_FilterOperator,_Filter,_Fragment,_History,_UIComponent) {
+        "use strict";
+
+        return Controller.extend("moc.controller.View1", {
+            onInit: function () {
+
+            }
+        });
+    });
---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["moc/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
